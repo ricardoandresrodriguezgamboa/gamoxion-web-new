5fae9fb01e441756272915ed5c5dc5c24e34751a
Ricardo Rodriguez G <ricardoandres749@gmail.com>
2025-09-06 22:28:22 -0400

    fix contact form

--- php/form.php
+++ php/form.php
@@ -1,129 +1,115 @@
 <?php
- 
- use PHPMailer\PHPMailer\PHPMailer;
- use PHPMailer\PHPMailer\Exception;
- 
- require 'Exception.php';
- require 'PHPMailer.php';
- require 'SMTP.php';
- 
- $mail = new PHPMailer(true);                              // Passing `true` enables exceptions
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
+require 'Exception.php';
+require 'PHPMailer.php';
+require 'SMTP.php';
 
- if($_SERVER['REQUEST_METHOD']=="POST"){
+$mail = new PHPMailer(true);
 
-  if(isset($_POST['g-recaptcha-response'])){
-      $token = $_POST['g-recaptcha-response'];
-      $url = 'https://www.google.com/recaptcha/api/siteverify';
-      $data = array(
-          'secret' => '6LeI2MArAAAAAKmVGy1ZqFYklHWSxQQJXj98rgM9',
-          'response' => $token
-      );
+if ($_SERVER['REQUEST_METHOD'] === "POST") {
 
-      $options = array(
-          'http' => array (
-              'header' => "Content-Type: application/x-www-form-urlencoded\r\n",
-              'method' => 'POST',
-              'content' => http_build_query($data)
-          )
-      );
+    if (isset($_POST['g-recaptcha-response'])) {
+        $token = $_POST['g-recaptcha-response'];
+        $url = 'https://www.google.com/recaptcha/api/siteverify';
+        $data = [
+            'secret' => '6LeI2MArAAAAAKmVGy1ZqFYklHWSxQQJXj98rgM9',
+            'response' => $token
+        ];
 
-      $context  = stream_context_create($options);
-      $result = file_get_contents($url, false, $context);
-      $response = json_decode($result);
+        $options = [
+            'http' => [
+                'header'  => "Content-Type: application/x-www-form-urlencoded\r\n",
+                'method'  => 'POST',
+                'content' => http_build_query($data)
+            ]
+        ];
 
-      /*
-      - google response score is between 0.0 to 1.0
-      - if score is 0.5, it's a human
-      - if score is 0.0, it's a bot
-      - google recommend to use score 0.5 for verify human
-      */
-      if ($response->success && $response->score >= 0.5) {
-        
+        $context  = stream_context_create($options);
+        $result = file_get_contents($url, false, $context);
+        $response = json_decode($result);
 
+        // Verificar éxito, score y acción
+        if ($response->success && $response->score >= 0.5 && $response->action === 'submit') {
 
-        try {
+            try {
+                // Escapar inputs para mayor seguridad
+                $nombre   = htmlspecialchars($_POST['name'], ENT_QUOTES, 'UTF-8');
+                $telefono = htmlspecialchars($_POST['phone'], ENT_QUOTES, 'UTF-8');
+                $email    = htmlspecialchars($_POST['email'], ENT_QUOTES, 'UTF-8');
+                $mensaje  = htmlspecialchars($_POST['message'], ENT_QUOTES, 'UTF-8');
+                $asunto   = htmlspecialchars($_POST['subject'], ENT_QUOTES, 'UTF-8');
 
-          $nombre = $_POST['name'];
-          $telefono = $_POST['phone'];   
-          $email = $_POST['email'];
-          $mensaje = $_POST['message'];
-          $asunto = $_POST['subject'];
+                // Validar que no estén vacíos
+                if (!empty($nombre) && !empty($telefono) && !empty($email) && !empty($mensaje) && !empty($asunto)) {
 
-          if (isset($nombre) && !empty($nombre) &&
-          isset($telefono) && !empty($telefono) &&
-          isset($email) && !empty($email) &&
-          isset($mensaje) && !empty($mensaje) &&
-          isset($asunto) && !empty($asunto)) {
+                    // Configuración PHPMailer
+                    $mail->SMTPDebug = 0;
+                    $mail->isSMTP();
+                    $mail->Host = 'mail.gamoxion.com';
+                    $mail->SMTPAuth = true;
+                    $mail->Username = 'contacto@gamoxion.com';
+                    $mail->Password = 'B)PMS!o$6uE,v1ZA';
+                    $mail->SMTPSecure = 'ssl';
+                    $mail->Port = 465;
 
+                    // Remitente y destinatarios
+                    $mail->setFrom('contacto@gamoxion.com', 'Gamoxion');
+                    $mail->addAddress('contacto@gamoxion.com');
+                    $mail->addAddress('ricardoandres749@gmail.com');
+                    $mail->addAddress('pablorodrigueza@hotmail.cl');
 
+                    // Contenido del email
+                    $contenido = "<b>Detalles del Contacto:</b><br><br>
+                                  <b>Nombre:</b> $nombre<br>
+                                  <b>Teléfono:</b> $telefono<br>
+                                  <b>Correo:</b> $email<br>
+                                  <b>Asunto:</b> $asunto<br>
+                                  <b>Mensaje:</b> $mensaje.";
 
-            
-           //Server settings
-           $mail->SMTPDebug = 0;                                  // Enable verbose debug output SET TO 0
-           $mail->isSMTP();                                      // Set mailer to use SMTP
-           $mail->Host = 'mail.gamoxion.com';                   // Specify main and backup SMTP servers
-           $mail->SMTPAuth = true;                               // Enable SMTP authentication
-           $mail->Username = 'contacto@gamoxion.com';                 // SMTP username
-           $mail->Password = 'B)PMS!o$6uE,v1ZA';                           // SMTP password
-           $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
-           $mail->Port = 465;                                    // TCP port to connect to
-       
-           //Recipients
-       
-           //Desde
-           $mail->setFrom('contacto@gamoxion.com','Gamoxion');
-           
-           //Para
-        
-           $mail->addAddress('contacto@gamoxion.com');     
-           
-           $mail->addAddress('ricardoandres749@gmail.com'); 
-           $mail->addAddress('pablorodrigueza@hotmail.cl'); 
-      
-          
-       
-       
-      
-         $contenido = '<b>Detalles del Contacto. </b> <br><br> <b>Nombre: </b>' .$nombre. '<br> <b>Telefono: </b>' .$telefono. '<br> <b>Correo: </b>'. $email . '<br> <b>Asunto: </b>'. $asunto. '<br> <b>Mensaje: </b>' .$mensaje. '.';
-         $contenidosinhtml = 'Detalles del Contacto. Nombre: ' .$nombre. ',Telefono: ' .$telefono. ',Correo: '. $email. ',Asunto:'. $asunto. ',Mensaje:' .$mensaje. '.';
-           
-           
-           //Content
-           $mail->isHTML(true);                                  // Set email format to HTML
-           $mail->Subject = 'Contacto Gamoxion - ' .$asunto. ' - ' .$nombre. '.';
-           $mail->Body    = $contenido;
-           $mail->AltBody = $contenidosinhtml;
-      
-          // Specify UTF-8 encoding
-          $mail->CharSet = 'UTF-8';
-       
-           $mail->send();
-           echo "<script type='text/javascript'>alert('Gracias por contactarnos, le responderemos a la brevedad.');location.replace(document.referrer); </script>";
+                    $contenidosinhtml = "Detalles del Contacto. Nombre: $nombre, Teléfono: $telefono, Correo: $email, Asunto: $asunto, Mensaje: $mensaje.";
 
-          //  echo '{ "alert": "alert alert-success alert-dismissable", "message": "Your message has been sent successfully!" }';
+                    $mail->isHTML(true);
+                    $mail->Subject = "Contacto Gamoxion - $asunto - $nombre";
+                    $mail->Body    = $contenido;
+                    $mail->AltBody = $contenidosinhtml;
+                    $mail->CharSet = 'UTF-8';
 
+                    $mail->send();
 
-          }else{
-            echo "<script type='text/javascript'>alert('Debe completar todos los campos del formulario.');location.replace('http://gamoxion.com/#contacto');</script>";
-          }
-      
-      
-       
-      
-       } catch (Exception $e) {
-            echo "<script type='text/javascript'>alert('Ha ocurrido un error al contactarse, intente mas tarde.');location.replace(document.referrer);</script>";
-       }
+                    echo "<script type='text/javascript'>
+                            alert('Gracias por contactarnos, le responderemos a la brevedad.');
+                            location.replace(document.referrer);
+                          </script>";
 
-  
-      } else {
-          echo json_encode(array('success' => false, "msg"=>"You are a robot!", "response"=>$response));
-      }
-  }
+                } else {
+                    echo "<script type='text/javascript'>
+                            alert('Debe completar todos los campos del formulario.');
+                            location.replace('http://gamoxion.com/#contacto');
+                          </script>";
+                }
+
+            } catch (Exception $e) {
+                echo "<script type='text/javascript'>
+                        alert('Ha ocurrido un error al enviar el mensaje. Intente más tarde.');
+                        location.replace(document.referrer);
+                      </script>";
+            }
+
+        } else {
+            // Si falla reCAPTCHA
+            echo "<script type='text/javascript'>
+                    alert('No se pudo verificar que seas humano. Intenta de nuevo.');
+                    location.replace(document.referrer);
+                  </script>";
+        }
+    } else {
+        // Si no hay token
+        echo "<script type='text/javascript'>
+                alert('No se recibió el token de reCAPTCHA.');
+                location.replace(document.referrer);
+              </script>";
+    }
 }
-
-
-
-
- 
- ?>
+?>
